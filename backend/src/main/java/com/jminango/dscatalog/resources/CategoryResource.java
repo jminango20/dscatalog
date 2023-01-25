059bef8268ccbe33ac90357b06650fe7ff9d99c4
jminango <juan.minango@bbchain.com.br>
2023-01-25 11:07:23 -0300

    h2 database

--- backend/src/main/java/com/jminango/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/jminango/dscatalog/resources/CategoryResource.java
@@ -1,24 +1,24 @@
 package com.jminango.dscatalog.resources;
 
 import com.jminango.dscatalog.entities.Category;
+import com.jminango.dscatalog.servicies.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
+    @Autowired
+    private CategoryService service;
     @GetMapping
     public ResponseEntity<List<Category>> findAll(){
-        List<Category> listCategory = new ArrayList<>();
-        listCategory.add(new Category(1L, "notebook"));
-        listCategory.add(new Category(2L, "phone"));
-        return ResponseEntity.ok().body(listCategory);
+        return ResponseEntity.ok().body(service.findAll());
     }
 
 
